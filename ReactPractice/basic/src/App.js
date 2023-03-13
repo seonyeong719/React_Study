4f7f86c6a188b5b18b2895844a90b74590c80c5c
seonyeong719 <sy904280@gmail.com>
2023-03-13 20:24:20 +0900

    Add: react 기초 중첩객체 복습

--- ReactPractice/basic/src/App.js
+++ ReactPractice/basic/src/App.js
@@ -1,9 +1,11 @@
-import Cursor from "./components/point";
+import Cursor from "./components/mouse/Point";
+import TextChange from "./page/Changetxt";
 
 function App() {
   return (
     <>
-      <Cursor />
+      <TextChange />
+      {/* <Cursor /> */}
     </>
   );
 }
