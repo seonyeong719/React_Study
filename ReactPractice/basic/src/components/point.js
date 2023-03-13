11fd6c29e3a79d368abeac3f71ca3709037a7811
seonyeong719 <sy904280@gmail.com>
2023-03-13 18:33:01 +0900

    Feat: useSate를 사용한 커서 이벤트 구현

--- ReactPractice/basic/src/components/point.js
+++ ReactPractice/basic/src/components/point.js
@@ -1,10 +1,27 @@
+import { useState } from "react";
 import styled from "styled-components";
 
 function Cursor() {
+  //   const [x, setX] = useState(0);
+  //   const [y, setY] = useState(0);
+  const [xyPosition, setXyPosition] = useState({ x: 0, y: 0 });
+
+  const cursorMove = (e) => {
+    // console.log(e.clientX, e.clientY);
+    // setX(e.clientX);
+    // setY(e.clientY);
+    setXyPosition({ x: e.clientX, y: e.clientY });
+
+    // setXyPosition((prev) => ({ ...prev, x: e.clientX }));
+    // spread 복사를 이용해 수평으로만 이동 가능하게 만들었다.
+    // useState 함수를 굳이 2개를 선언해줄 필요 없이 한번에 객체에 담아서 사용 가능.
+    // 또한, 깊은복사를 통해 다양하게 수직, 수평 기능 구현이 가능하다.
+  };
   return (
     <>
-      <Container>
-        <Point />
+      <Container onPointerMove={cursorMove}>
+        {/* <Point style={{ transform: `translate(${x}px,${y}px)` }} /> */}
+        <Point style={{ transform: `translate(${xyPosition.x}px,${xyPosition.y}px)` }} />
       </Container>
     </>
   );
